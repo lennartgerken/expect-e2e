c1914ffebb7843aa849b2ef2048d4951d89b2e45
Lennart Gerken <49632312+lennartgerken@users.noreply.github.com>
2026-03-14 19:51:29 +0000

    Configure eslint for tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,24 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import nextTs from 'eslint-config-next/typescript'
+import tseslint from 'typescript-eslint'
+import playwright from 'eslint-plugin-playwright'
+import eslint from '@eslint/js'
 
 const eslintConfig = defineConfig([
-    ...nextVitals,
-    ...nextTs,
+    eslint.configs.recommended,
+    {
+        extends: [nextVitals, nextTs],
+        ignores: ['test-demos/**']
+    },
+    {
+        extends: [tseslint.configs.recommended, tseslint.configs.stylistic],
+        files: ['test-demos/**']
+    },
+    {
+        extends: [playwright.configs['flat/recommended']],
+        files: ['test-demos/playwright/**']
+    },
     globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts'])
 ])
 
